62c83fb3d1cecfa3715739df20ba84efd6965463
satyam <satyamdigital34@gmail.com>
2023-11-11 01:46:08 +0530

    practice

--- Heap/prac3.cpp
+++ Heap/prac3.cpp
@@ -2,6 +2,27 @@
 using namespace std;
 int main()
 {
+    int b[] = {1, 5, 7, 3, 80};
+    int a = 2;
+    int profit = 0;
+    priority_queue<int> maxheap;
+    for (auto i : b)
+    {
+        maxheap.push(i);
+    }
+    int curr = -1;
+    while (a != 0)
+    {
+        curr = maxheap.top();
+        profit += curr;
+        maxheap.pop();
+
+        maxheap.push(curr / 2);
+
+        a--;
+    }
+
+    cout << "The maximum number of chocolates that you can eat is " << profit;
 
     return 0;
 }
